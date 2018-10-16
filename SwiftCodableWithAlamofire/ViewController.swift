2c1a0fd9920896d6537969796146ffa5f90f076f
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-10-16 03:13:33 -0700

    Initial Commit

---
+++ SwiftCodableWithAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCodableWithAlamofire
+//
+//  Created by Raghavendra Dattawad on 10/16/18.
+//  Copyright © 2018 Raghavendra Dattawad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
